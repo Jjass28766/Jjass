f887aa883fa7db018c73da43ca06e3e6a26cb6a0
Jjass28766 <ongtangcog@gmail.com>
2024-11-02 20:43:49 +0800

    Update BrixMODZ1.lua

--- BrixMODZ1.lua
+++ BrixMODZ1.lua
@@ -1265,7 +1265,7 @@ end
 
 function reload()
 local so = gg.getRangesList('libunity.so')[1].start
-local py = 0x40435BC
+local py = 0x8FC2374
 gg.setValues({{address = so + py, flags = gg.TYPE_QWORD, value = "h002C40BCC0035FD6"}})
 gg.toast("NO RELOAD ACTIVATED")
 end
